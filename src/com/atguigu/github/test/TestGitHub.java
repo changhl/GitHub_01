b0653f7777d2681a8f1cf82c0575cd69c4ba0b89
changhl <447239037@qq.com>
2016-09-18 20:09:17 +0800

    test2
    test2

--- src/com/atguigu/github/test/TestGitHub.java
+++ src/com/atguigu/github/test/TestGitHub.java
@@ -5,5 +5,6 @@ public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("Hi , Mr ZHOU");
 		System.out.println("hello , 0427");
+		System.out.println("hello2 , 这里是测试");
 	}
 }
